9ba99934a9722728f32e89a9976457d8bc2c4815
v4dmz <143401420+v4dmz@users.noreply.github.com>
2024-02-29 11:29:06 -0800

    JavaPt1

---
+++ new.js
@@ -0,0 +1,26 @@
+var num = 6;
+console.log(num); 
+num++; 
+console.log(num); 
+console.log(num++);
+console.log(num); 
+++num; 
+console.log(num); 
+console.log(++num);  
+
+
+
+
+var number = 6;
+number = number + 5;
+number += 5; 
+number -= 5; 
+number *= 5;
+number /= 5;
+
+5  + '5' 
+5 * '5' 
+5 * '' 
+
+var val1 = +prompt('Enter Value'); 
+console.log(val1 + val2);
